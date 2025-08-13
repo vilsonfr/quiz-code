3050c570575324f60ac7748c309c161ca74967fd
infralognex <133527230+infralognex@users.noreply.github.com>
2025-08-12 21:34:36 -0300

    ajust

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,5 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -11,7 +8,6 @@ plugins {
 android {
     namespace = "com.alamidia.quizcode.slot"
     compileSdk = flutter.compileSdkVersion
-    
     ndkVersion = "27.0.12077973"
 
     compileOptions {
--- android/app/src/main/kotlin/com/alamidia/quizcode/slot/MainActivity.kt
+++ android/app/src/main/kotlin/com/alamidia/quizcode/slot/MainActivity.kt
@@ -2,4 +2,4 @@ package com.alamidia.quizcode.slot
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() 
